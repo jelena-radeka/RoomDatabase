d870a931aee94deca023e3ce6fcd65d366a6ae4b
radeka <jelenaradeka87@gmail.com>
2021-09-09 09:20:34 +0200

    small changes

--- app/src/main/java/com/sams3p10l/roomapptest/acivity/AddNewActivity.java
+++ app/src/main/java/com/sams3p10l/roomapptest/acivity/AddNewActivity.java
@@ -1,6 +1,4 @@
-package com.sams3p10l.roomapptest;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.sams3p10l.roomapptest.acivity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,19 +6,25 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sams3p10l.roomapptest.AppExecutors;
+import com.sams3p10l.roomapptest.R;
+import com.sams3p10l.roomapptest.db.RoomDB;
+import com.sams3p10l.roomapptest.model.ItemData;
+
 public class AddNewActivity extends AppCompatActivity {
 
-
-    public static final String TITLE ="title";
-    EditText editText;
-    Button btn;
+    public static final String TITLE = "title";
+    private EditText editText;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new);
-        editText=findViewById(R.id.edit_text);
-        btn=findViewById(R.id.btn_add);
+        editText = findViewById(R.id.edit_text);
+        btn = findViewById(R.id.btn_add);
         btn.setOnClickListener(v -> saveItem());
     }
 
@@ -30,7 +34,6 @@ public class AddNewActivity extends AppCompatActivity {
             Toast.makeText(this, "Empty item", Toast.LENGTH_SHORT).show();
             return;
         }
-
         Intent data = new Intent();
         data.putExtra(TITLE, title);
         setResult(RESULT_OK, data);
